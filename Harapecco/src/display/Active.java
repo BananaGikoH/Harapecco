9cd6c321cebebb72d903e41a2e2e39ee7ea934a7
BananaGikoH <sunlight.cheerful.asayake@gmail.com>
2016-05-26 01:12:19 +0900

    Display making
    全てGridBagLayoutに変更。
    文字の間隔を調整。(今は広すぎ？)
    今後は
    ・forで処理をまとめる（メソッド化？）
    ・この問い以外の質問も記述する

--- Harapecco/src/display/Active.java
+++ Harapecco/src/display/Active.java
@@ -1,15 +1,16 @@
 package display;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
 
 public class Active extends JFrame{
 
@@ -27,31 +28,32 @@ public class Active extends JFrame{
 		JPanel TodaysInfo = new JPanel();
 
 		JLabel TodaysDate = new JLabel("X月X日の日記を編集しています...");
-		TodaysInfo.add(TodaysDate,BorderLayout.NORTH);
+		TodaysInfo.add(TodaysDate,BorderLayout.WEST);
 
 		display.Active p = new display.Active();
 		//
 		JPanel ParsonalEvent = new JPanel();
 		ParsonalEvent = p.ParsonalEvent();
 
-		SpringLayout ActivePanelLayout = new SpringLayout();
+		GridBagLayout gbLayout = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
 		JPanel ActivePanel = new JPanel();
 
-		ActivePanel.setLayout(ActivePanelLayout);
+		ActivePanel.setLayout(gbLayout);
 
-		ActivePanelLayout.putConstraint(SpringLayout.NORTH, TodaysInfo, 10, SpringLayout.NORTH, ActivePanel);
-		ActivePanelLayout.putConstraint(SpringLayout.WEST, TodaysInfo, 10, SpringLayout.WEST, ActivePanel);
-		ActivePanelLayout.putConstraint(SpringLayout.NORTH, ParsonalEvent, 0, SpringLayout.SOUTH, ActivePanel);
-		ActivePanelLayout.putConstraint(SpringLayout.WEST, ParsonalEvent, 0, SpringLayout.WEST, ActivePanel);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbLayout.setConstraints(TodaysInfo, gbc);
 
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbLayout.setConstraints(ParsonalEvent, gbc);
 
 		ActivePanel.add(TodaysInfo);
 		ActivePanel.add(ParsonalEvent);
 
-
 		ActivePanel.list();
-		ActivePanel.setBackground(Color.blue);
-		//SpringLayoutにすると表示されなくなる…どうして？
+		//ActivePanel.setBackground(Color.GRAY);
 
 		return ActivePanel;
 	}
@@ -61,10 +63,8 @@ public class Active extends JFrame{
 	int ParsonalEventRC = 3;
 
 	JPanel ParsonalEvent(){
+
 		JPanel ParsonalEvent = new JPanel();
-		//
-		SpringLayout ParsonalEventLayout = new SpringLayout();
-		ParsonalEvent.setLayout(ParsonalEventLayout);
 
 		JLabel Sectiontitle = new JLabel("今日の主な出来事");
 		JLabel Summary = new JLabel("今日の自分の出来事を書き込む");
@@ -74,12 +74,25 @@ public class Active extends JFrame{
 		display.Active es = new display.Active();
 		EntrySheet = es.ParsonalEventEntrySheetConstruct(ParsonalEventRC);
 
-		ParsonalEventLayout.putConstraint(SpringLayout.NORTH, Sectiontitle, 5, SpringLayout.NORTH, ParsonalEvent);
-		ParsonalEventLayout.putConstraint(SpringLayout.WEST, Sectiontitle, 5, SpringLayout.WEST, ParsonalEvent);
-		ParsonalEventLayout.putConstraint(SpringLayout.NORTH, Summary, 5, SpringLayout.SOUTH, Sectiontitle);
-		ParsonalEventLayout.putConstraint(SpringLayout.WEST, Summary, 20, SpringLayout.WEST, ParsonalEvent);
-		ParsonalEventLayout.putConstraint(SpringLayout.NORTH, EntrySheet, 5, SpringLayout.SOUTH, Summary);
-		ParsonalEventLayout.putConstraint(SpringLayout.WEST, EntrySheet, 20, SpringLayout.WEST, Summary);
+		GridBagLayout gbLayout = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		ParsonalEvent.setLayout(gbLayout);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbLayout.setConstraints(Sectiontitle, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbLayout.setConstraints(Summary, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbLayout.setConstraints(EntrySheet, gbc);
 
 		ParsonalEvent.add(Sectiontitle);
 		ParsonalEvent.add(Summary);
@@ -91,69 +104,141 @@ public class Active extends JFrame{
 	JPanel ParsonalEventEntrySheetConstruct(int ParsonalEventRC){
 
 		//-----------1--------------------
-		JPanel cell_1 = new JPanel();
-		SpringLayout layout_1 = new SpringLayout();
-		cell_1.setLayout(layout_1);
 
-		JLabel SheetIndex_1 = new JLabel("1:");
+		JLabel SheetIndex_1 = new JLabel("1 : ");
 		//項
 		JTextField Title_1 = new JTextField();
+		Title_1.setColumns(15);
 		JTextArea EntryBox_1 = new JTextArea("",1,20);
 		EntryBox_1.setLineWrap(true);
 
-		layout_1.putConstraint(SpringLayout.NORTH, SheetIndex_1, 0, SpringLayout.NORTH, cell_1);
-		layout_1.putConstraint(SpringLayout.WEST, SheetIndex_1, 0, SpringLayout.WEST, cell_1);
-		layout_1.putConstraint(SpringLayout.NORTH, Title_1, 0, SpringLayout.NORTH, SheetIndex_1);
-		layout_1.putConstraint(SpringLayout.WEST, Title_1, 0, SpringLayout.WEST, SheetIndex_1);
-		layout_1.putConstraint(SpringLayout.NORTH, EntryBox_1, 5, SpringLayout.SOUTH, Title_1);
-		layout_1.putConstraint(SpringLayout.WEST, EntryBox_1, 20, SpringLayout.WEST, cell_1);
+		GridBagLayout gbLayout = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
 
-		cell_1.add(SheetIndex_1);
-		cell_1.add(Title_1);
+		JPanel minicell_1 = new JPanel();
+		minicell_1.setLayout(gbLayout);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(SheetIndex_1, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(Title_1, gbc);
+
+		minicell_1.add(SheetIndex_1);
+		minicell_1.add(Title_1);
+
+		JPanel cell_1 = new JPanel();
+		cell_1.setLayout(gbLayout);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(minicell_1, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(EntryBox_1, gbc);
+
+		cell_1.add(minicell_1);
 		cell_1.add(EntryBox_1);
 
-		//-----------2--------------------
-		JPanel cell_2 = new JPanel();
-		SpringLayout layout_2 = new SpringLayout();
-		cell_2.setLayout(layout_2);
+		//-----------1--------------------
 
-		JLabel SheetIndex_2 = new JLabel("2:");
+		JLabel SheetIndex_2 = new JLabel("2 : ");
 		//項
 		JTextField Title_2 = new JTextField();
+		Title_2.setColumns(15);
 		JTextArea EntryBox_2 = new JTextArea("",1,20);
 		EntryBox_2.setLineWrap(true);
 
-		layout_2.putConstraint(SpringLayout.NORTH, SheetIndex_2, 0, SpringLayout.NORTH, cell_2);
-		layout_2.putConstraint(SpringLayout.WEST, SheetIndex_2, 0, SpringLayout.WEST, cell_2);
-		layout_2.putConstraint(SpringLayout.NORTH, Title_2, 0, SpringLayout.NORTH, SheetIndex_2);
-		layout_2.putConstraint(SpringLayout.WEST, Title_2, 0, SpringLayout.WEST, SheetIndex_2);
-		layout_2.putConstraint(SpringLayout.NORTH, EntryBox_2, 5, SpringLayout.SOUTH, Title_2);
-		layout_2.putConstraint(SpringLayout.WEST, EntryBox_2, 20, SpringLayout.WEST, cell_2);
+		JPanel minicell_2 = new JPanel();
+		minicell_2.setLayout(gbLayout);
 
-		cell_2.add(SheetIndex_2);
-		cell_2.add(Title_2);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(SheetIndex_2, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(Title_2, gbc);
+
+		minicell_2.add(SheetIndex_2);
+		minicell_2.add(Title_2);
+
+		JPanel cell_2 = new JPanel();
+		cell_2.setLayout(gbLayout);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(minicell_2, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(EntryBox_2, gbc);
+
+		cell_2.add(minicell_2);
 		cell_2.add(EntryBox_2);
 
-		//-----------3--------------------
-		JPanel cell_3 = new JPanel();
-		SpringLayout layout_3 = new SpringLayout();
-		cell_3.setLayout(layout_3);
+		//-----------1--------------------
 
-		JLabel SheetIndex_3 = new JLabel("3:");
+		JLabel SheetIndex_3 = new JLabel("3 : ");
 		//項
 		JTextField Title_3 = new JTextField();
+		Title_3.setColumns(15);
 		JTextArea EntryBox_3 = new JTextArea("",1,20);
 		EntryBox_3.setLineWrap(true);
 
-		layout_3.putConstraint(SpringLayout.NORTH, SheetIndex_3, 0, SpringLayout.NORTH, cell_3);
-		layout_3.putConstraint(SpringLayout.WEST, SheetIndex_3, 0, SpringLayout.WEST, cell_3);
-		layout_3.putConstraint(SpringLayout.NORTH, Title_3, 0, SpringLayout.NORTH, SheetIndex_3);
-		layout_3.putConstraint(SpringLayout.WEST, Title_3, 0, SpringLayout.WEST, SheetIndex_3);
-		layout_3.putConstraint(SpringLayout.NORTH, EntryBox_3, 5, SpringLayout.SOUTH, Title_3);
-		layout_3.putConstraint(SpringLayout.WEST, EntryBox_3, 20, SpringLayout.WEST, cell_3);
+		JPanel minicell_3 = new JPanel();
+		minicell_3.setLayout(gbLayout);
 
-		cell_3.add(SheetIndex_3);
-		cell_3.add(Title_3);
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(SheetIndex_3, gbc);
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(Title_3, gbc);
+
+		minicell_3.add(SheetIndex_3);
+		minicell_3.add(Title_3);
+
+		JPanel cell_3 = new JPanel();
+		cell_3.setLayout(gbLayout);
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(minicell_3, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		gbc.insets = new Insets(5, 5, 5, 5);
+		gbLayout.setConstraints(EntryBox_3, gbc);
+
+		cell_3.add(minicell_3);
 		cell_3.add(EntryBox_3);
 
 		//--------------------------------
@@ -166,18 +251,27 @@ public class Active extends JFrame{
 		sheet.add(cell_1);
 		sheet.add(cell_2);
 		sheet.add(cell_3);
-		/*
-		SpringLayout sheetlayout = new SpringLayout();
+
+		GridBagLayout sheetlayout = new GridBagLayout();
+		GridBagConstraints SheetGbc = new GridBagConstraints();
+
 		sheet.setLayout(sheetlayout);
 
-		sheetlayout.putConstraint(SpringLayout.NORTH, cell_1, 0, SpringLayout.NORTH, sheet);
-		sheetlayout.putConstraint(SpringLayout.WEST, cell_1, 0, SpringLayout.WEST, sheet);
-		sheetlayout.putConstraint(SpringLayout.NORTH, cell_2, 0, SpringLayout.NORTH, cell_2);
-		sheetlayout.putConstraint(SpringLayout.WEST, cell_2, 0, SpringLayout.WEST, sheet);
-		sheetlayout.putConstraint(SpringLayout.NORTH, cell_3, 0, SpringLayout.NORTH, cell_3);
-		sheetlayout.putConstraint(SpringLayout.WEST, cell_3, 0, SpringLayout.WEST, sheet);
+		SheetGbc.gridx = 0;
+		SheetGbc.gridy = 0;
+		SheetGbc.anchor = GridBagConstraints.NORTHWEST;
+		sheetlayout.setConstraints(cell_1, SheetGbc);
 
-		*/
+		SheetGbc.gridx = 0;
+		SheetGbc.gridy = 1;
+		SheetGbc.anchor = GridBagConstraints.NORTHWEST;
+		sheetlayout.setConstraints(cell_2, SheetGbc);
+
+		SheetGbc.gridx = 0;
+		SheetGbc.gridy = 2;
+		SheetGbc.anchor = GridBagConstraints.NORTHWEST;
+		sheetlayout.setConstraints(cell_3, SheetGbc);
+
 		return sheet;
 	}
 }
